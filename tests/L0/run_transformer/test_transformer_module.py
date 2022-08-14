4736678fe69625c40c2c400ca2da97f92930a737
Masaki Kozuki <mkozuki@nvidia.com>
2022-08-13 19:01:14 -0700

    manually define
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- tests/L0/run_transformer/test_transformer_module.py
+++ tests/L0/run_transformer/test_transformer_module.py
@@ -31,68 +31,65 @@ def get_launch_option(test_filename) -> Tuple[bool, str]:
     return should_skip, ""
 
 
-# def run_transformer_tests(test_file_path):
-def define_test_methods(cls):
+def get_test_command(test_file: str) -> str:
+    python_executable_path = sys.executable
+    is_denied = False
+    should_skip, launch_option = get_launch_option(test_file)
+    if should_skip:
+        print(
+            f"### {i} / {len(files)}: {test_file} skipped. Requires multiple GPUs."
+        )
+        return ""
+    test_run_cmd = (
+        f"{python_executable_path} {launch_option} {test_file} "
+        "--micro-batch-size 2 --num-layers 16 --hidden-size 256 --num-attention-heads 8 --max-position-embeddings "
+        "512 --seq-length 512 --global-batch-size 128"
+    )
+    if "bert" in test_file or "gpt" in test_file:
+        import torch
+
+        num_devices = torch.cuda.device_count()
+        if "bert" in test_file:
+            # "bert" uses the interleaving.
+            tensor_model_parallel_size = 2 if num_devices % 2 == 0 and num_devices > 4 else 1
+        if "gpt" in test_file:
+            # "gpt" uses the non-interleaving.
+            tensor_model_parallel_size = 2 if num_devices % 2 == 0 and num_devices >= 4 else 1
+        pipeline_model_parallel_size = num_devices // tensor_model_parallel_size
+        test_run_cmd += f" --pipeline-model-parallel-size {pipeline_model_parallel_size} --tensor-model-parallel-size {tensor_model_parallel_size}"
+
+        if "bert" in test_file:
+            test_run_cmd += f" --bert-no-binary-head"
+    else:
+        test_run_cmd += f" --use-cpu-initialization"
+    return test_run_cmd
+
+
+def _get_test_file(key):
     python_executable_path = sys.executable
     directory = os.path.dirname(__file__)
-    files = [
+    test_file = [
         os.path.join(directory, f)
         for f in os.listdir(directory)
-        if f.startswith("run_") and os.path.isfile(os.path.join(directory, f))
-    ]
-    print("#######################################################")
-    print(f"# Python executable path: {python_executable_path}")
-    print(f"# {len(files)} tests: {files}")
-    print("#######################################################")
-    errors = []
-    for i, test_file in enumerate(files, 1):
-        is_denied = False
-        should_skip, launch_option = get_launch_option(test_file)
-        if should_skip:
-            print(
-                f"### {i} / {len(files)}: {test_file} skipped. Requires multiple GPUs."
-            )
-            continue
-        test_run_cmd = (
-            f"{python_executable_path} {launch_option} {test_file} "
-            "--micro-batch-size 2 --num-layers 16 --hidden-size 256 --num-attention-heads 8 --max-position-embeddings "
-            "512 --seq-length 512 --global-batch-size 128"
-        )
-        if "bert" in test_file or "gpt" in test_file:
-            import torch
-
-            num_devices = torch.cuda.device_count()
-            if "bert" in test_file:
-                # "bert" uses the interleaving.
-                tensor_model_parallel_size = 2 if num_devices % 2 == 0 and num_devices > 4 else 1
-            if "gpt" in test_file:
-                # "gpt" uses the non-interleaving.
-                tensor_model_parallel_size = 2 if num_devices % 2 == 0 and num_devices >= 4 else 1
-            pipeline_model_parallel_size = num_devices // tensor_model_parallel_size
-            test_run_cmd += f" --pipeline-model-parallel-size {pipeline_model_parallel_size} --tensor-model-parallel-size {tensor_model_parallel_size}"
-
-            if "bert" in test_file:
-                test_run_cmd += f" --bert-no-binary-head"
-        else:
-            test_run_cmd += f" --use-cpu-initialization"
-        print(f"### {i} / {len(files)}: cmd: {test_run_cmd}")
-
-        def test_method(self, *args, **kwargs):
-            subprocess.check_output(test_run_cmd, shell=True)
-
-        filename = os.path.splitext(os.path.basename(test_file))[0]
-        setattr(cls, f"test_{filename}", test_method)
-    return cls
+        if f.startswith("run_") and os.path.isfile(os.path.join(directory, f)) and key in f
+    ][0]
+    return test_file
 
 
 class TestTransformer(unittest.TestCase):
+    def _test_impl(self, key: str):
+        test_file = _get_test_file(key)
+        command = get_test_command(test_file)
+        if command:
+            subprocess.run(command, shell=True, check=True)
+        else:
+            self.skipTest("Appropriate command is not generated")
 
-    @classmethod
-    def setUpClass(cls):
-        define_test_methods(cls)
+    def test_standalone_bert(self):
+        self._test_impl("bert")
 
-    # def test_transformer(self):
-    #     run_transformer_tests()
+    def test_standalone_gpt(self):
+        self._test_impl("gpt")
 
 
 if __name__ == "__main__":
